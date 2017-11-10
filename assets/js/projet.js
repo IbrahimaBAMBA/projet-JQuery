65cd59a8f12e2f6ad3f0427c6acb24296266c44f
clement <clement.schuvey@gmail.com>
2017-11-10 11:41:25 +0100

    adazd

--- assets/js/projet.js
+++ assets/js/projet.js
@@ -1,16 +1,14 @@
 $(function(){
+  var tableau = [];
   $('button[name=addPanier]').click(function(){
     var ParentOfButton = $(this).parents('.article');
-    var refArticle = ParentOfButton.children('.reference').text();
-    var imgArticle = ParentOfButton.children('img').attr('src');
+    var refArticle = ParentOfButton.children('.reference').children('strong').children('.ref').text().toString();
+    var imgArticle = ParentOfButton.children('img').attr('src').toString();
     var descripArticle = ParentOfButton.children('.description').text();
     var price = ParentOfButton.children('.price').text();
-    var titleArticle = ParentOfButton.children('.titleArticle').text();
-    console.log(ParentOfButton);
-    console.log(refArticle);
-    console.log(titleArticle);
-    console.log(imgArticle);
-    console.log(descripArticle);
-    console.log(price);
+    var titleArticle = ParentOfButton.children('.titleArticle').text().toString();
+    eval('var ' + refArticle + ' ={ titre:titleArticle, img:imgArticle, desp:descripArticle, price:price};');
+    tableau.push(eval(refArticle));
   });
+
 });
