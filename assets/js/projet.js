aef348c99ac5573ca0b1c00369c2d7a78e0b7aa5
clement <clement.schuvey@gmail.com>
2017-11-10 14:09:36 +0100

    fedfz

--- assets/js/projet.js
+++ assets/js/projet.js
@@ -2,38 +2,13 @@ $(function(){
   var tableau = [];
   $('button[name=addPanier]').click(function(){
     var ParentOfButton = $(this).parents('.article');
-    var refArticle = ParentOfButton.children('.reference').children('strong').children('.ref').text().toString();
-    var imgArticle = ParentOfButton.children('img').attr('src').toString();
+    var refArticle = ParentOfButton.children('.reference').children('strong').children('.ref').text();
+    var imgArticle = ParentOfButton.children('img').attr('src');
     var descripArticle = ParentOfButton.children('.description').text();
     var price = ParentOfButton.children('.price').text();
-    var titleArticle = ParentOfButton.children('.titleArticle').text().toString();
+    var titleArticle = ParentOfButton.children('.titleArticle').text();
     eval('var ' + refArticle + ' ={ titre:titleArticle, img:imgArticle, desp:descripArticle, price:price};');
     tableau.push(eval(refArticle));
-  var NombreArticleSelec = 0;
-  $('.article').click(function(){
-    var article = $(this).attr('id');
-    var imgArticle = $(this).children('img').attr('src');
-    var descripArticle = $(this).children('.description').text();
-    NombreArticleSelec++;
-    $('<div/>',{
-      id: 'ArticlePanierNum'+NombreArticleSelec,
-      class: 'articleDansPanier',
-    }).appendTo('#panier');
-    $('<img/>',{
-      src: imgArticle,
-      class: 'imageDansPanier'
-    }).appendTo('#ArticlePanierNum'+NombreArticleSelec);
-    $('<p/>',{
-      text: descripArticle,
-      class:'descriptionDansPanier'
-    }).appendTo('#ArticlePanierNum'+NombreArticleSelec);
+    console.log(tableau);
   });
-});
-$(function(){
-$('img').mouseover(function(){
-  $(this).css('width','270px');   <!--'this' sert à repeter ce qui a été fait-->
-  $(this).mouseout(function(){
-  $(this).css('width', '220px');
-  });
-});
 });
