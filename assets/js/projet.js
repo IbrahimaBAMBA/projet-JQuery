d755a38ab43cd686aa14a6923a6215013a127898
clement <clement.schuvey@gmail.com>
2017-11-10 15:33:26 +0100

    changement html

--- assets/js/projet.js
+++ assets/js/projet.js
@@ -2,13 +2,19 @@ $(function(){
   var panier = [];
   $('button[name=addPanier]').click(function(){
     var ParentOfButton = $(this).parents('.article');
-    var refArticle = $('.ref').text();
-    var imgArticle = ParentOfButton.children('img').attr('src');
-    var descripArticle = $('.description').text();
-    var price = $('.price').text();
-    var titleArticle = $('.titre').text();
+    var refArticle = ParentOfButton.children('.reference').children('strong').children('.ref').text().toString();
+    var imgArticle = ParentOfButton.children('img').attr('src').toString();
+    var descripArticle = ParentOfButton.children('.description').text();
+    var price = ParentOfButton.children('.price').text();
+    var titleArticle = ParentOfButton.children('.titreArticle').text().toString();
     eval('var ' + refArticle + ' ={ titre:titleArticle, img:imgArticle, desp:descripArticle, price:price};');
     panier.push(eval(refArticle));
-    console.log(panier);
+    console.log(eval(refArticle).titre);
+  //
+  //   jQuery.each( panier, function( i, val ) {
+  //   $(titre).appendTo( '#basket' );
+  // });
   });
+
+
 });
