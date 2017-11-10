cd6535a9b29103ed1f297e1ef8ab3130c4b4f058
clement <clement.schuvey@gmail.com>
2017-11-10 16:24:33 +0100

    Js qui avance

--- assets/js/projet.js
+++ assets/js/projet.js
@@ -2,19 +2,24 @@ $(function(){
   var panier = [];
   $('button[name=addPanier]').click(function(){
     var ParentOfButton = $(this).parents('.article');
-    var refArticle = ParentOfButton.children('.reference').children('strong').children('.ref').text().toString();
+    var refArticle = ParentOfButton.children('.reference').children('.ref').text().toString();
     var imgArticle = ParentOfButton.children('img').attr('src').toString();
     var descripArticle = ParentOfButton.children('.description').text();
     var price = ParentOfButton.children('.price').text();
     var titleArticle = ParentOfButton.children('.titreArticle').text().toString();
-    eval('var ' + refArticle + ' ={ titre:titleArticle, img:imgArticle, desp:descripArticle, price:price};');
+    eval('var ' + refArticle + ' ={ titre:titleArticle, img:imgArticle, descrip:descripArticle, price:price};');
     panier.push(eval(refArticle));
-    console.log(eval(refArticle).titre);
-  //
-  //   jQuery.each( panier, function( i, val ) {
-  //   $(titre).appendTo( '#basket' );
-  // });
+     jQuery.each( panier, function( i, val ) {
+       var imgg = val.img;
+       var titre = val.titre;
+       var descrip = val.descrip;
+       var price = val.price;
+      $('<p>' + titre + imgg + descrip + price + '</p>').appendTo('#basket');
+
+      //  for (var prop in val) {
+      //    console.log(`${prop} = ${val[prop]}`);
+      //  }
+     });
+
   });
-
-
 });
